725c732957633fd10e2ca8293bcb528d366c0a74
Manoj <84076478+Manoj546@users.noreply.github.com>
2023-01-26 00:38:36 +0530

    final

--- prg 7/red_black.cpp
+++ prg 7/red_black.cpp
@@ -1,146 +1,219 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct node
+struct Node
 {
-	int key;
-	int count;
-	struct node *left, *right;
+	int data;
+	Node *parent;
+	Node *left;
+	Node *right;
+	int color;
 };
 
-struct node *newNode(int item)
-{
-	struct node *temp = (struct node *)malloc(sizeof(struct node));
-	temp->key = item;
-	temp->left = temp->right = NULL;
-	temp->count = 2;
-	return temp;
-}
+typedef Node *NodePtr;
 
-void inorder(struct node *root)
+class RedBlackTree
 {
-	if (root != NULL)
+private:
+	NodePtr root;
+	NodePtr TNULL;
+
+	void insertFix(NodePtr k)
 	{
-		int n = root->count;
-		string s;
-		if (n == 1){
-			s = "red";
+		NodePtr u;
+		while (k->parent->color == 1)
+		{
+			if (k->parent == k->parent->parent->right)
+			{
+				u = k->parent->parent->left;
+				if (u->color == 1)
+				{
+					u->color = 0;
+					k->parent->color = 0;
+					k->parent->parent->color = 1;
+					k = k->parent->parent;
+				}
+				else
+				{
+					if (k == k->parent->left)
+					{
+						k = k->parent;
+						rightRotate(k);
+					}
+					k->parent->color = 0;
+					k->parent->parent->color = 1;
+					leftRotate(k->parent->parent);
+				}
+			}
+			else
+			{
+				u = k->parent->parent->right;
+
+				if (u->color == 1)
+				{
+					u->color = 0;
+					k->parent->color = 0;
+					k->parent->parent->color = 1;
+					k = k->parent->parent;
+				}
+				else
+				{
+					if (k == k->parent->right)
+					{
+						k = k->parent;
+						leftRotate(k);
+					}
+					k->parent->color = 0;
+					k->parent->parent->color = 1;
+					rightRotate(k->parent->parent);
+				}
+			}
+			if (k == root)
+				break;
 		}
-		else if (n == 2)
-			s = "black";
+		root->color = 0;
+	}
+
+	void printHelper(NodePtr root, string indent, bool last)
+	{
+		if (root != TNULL)
+		{
+			cout << indent;
+			if (last)
+			{
+				cout << "RIGHT----";
+				indent += "   ";
+			}
+			else
+			{
+				cout << "LEFT----";
+				indent += "|  ";
+			}
+
+			string sColor = root->color ? "RED" : "BLACK";
+			cout << root->data << "(" << sColor << ")" << endl;
+			printHelper(root->left, indent, false);
+			printHelper(root->right, indent, true);
+		}
+	}
+
+public:
+	RedBlackTree()
+	{
+		TNULL = new Node;
+		TNULL->color = 0;
+		TNULL->left = nullptr;
+		TNULL->right = nullptr;
+		root = TNULL;
+	}
+
+	void leftRotate(NodePtr x)
+	{
+		NodePtr y = x->right;
+		x->right = y->left;
+		if (y->left != TNULL)
+			y->left->parent = x;
+		y->parent = x->parent;
+		if (x->parent == nullptr)
+			this->root = y;
+		else if (x == x->parent->left)
+			x->parent->left = y;
 		else
-			s = "Black";
-		cout << root->key << "("
-			 << s << ") ";
-		inorder(root->left);
-		inorder(root->right);
+			x->parent->right = y;
+		y->left = x;
+		x->parent = y;
 	}
-}
 
-struct node *insert(struct node *node, int key)
-{
-	if (node == NULL)
-		return newNode(key);
-
-	if (key == node->key)
+	void rightRotate(NodePtr x)
 	{
-		(node->count)++;
-		return node;
+		NodePtr y = x->left;
+		x->left = y->right;
+		if (y->right != TNULL)
+			y->right->parent = x;
+		y->parent = x->parent;
+		if (x->parent == nullptr)
+			this->root = y;
+		else if (x == x->parent->right)
+			x->parent->right = y;
+		else
+			x->parent->left = y;
+		y->right = x;
+		x->parent = y;
 	}
 
-	if (key < node->key)
-		node->left = insert(node->left, key);
-	else
-		node->right = insert(node->right, key);
-
-	return node;
-}
-
-struct node *minValueNode(struct node *node)
-{
-	struct node *current = node;
-
-	while (current->left != NULL)
-		current = current->left;
-
-	return current;
-}
-
-struct node *deleteNode(struct node *root, int key)
-{
-	if (root == NULL)
-		return root;
-
-	if (key < root->key)
-		root->left = deleteNode(root->left, key);
-
-	else if (key > root->key)
-		root->right = deleteNode(root->right, key);
-
-	else
+	// Inserting a node
+	void insert(int key)
 	{
-		if (root->count > 1)
+		NodePtr node = new Node;
+		node->parent = nullptr;
+		node->data = key;
+		node->left = TNULL;
+		node->right = TNULL;
+		node->color = 1;
+		NodePtr y = nullptr;
+		NodePtr x = this->root;
+
+		while (x != TNULL)
 		{
-			(root->count)--;
-			return root;
+			y = x;
+			if (node->data < x->data)
+				x = x->left;
+			else
+				x = x->right;
 		}
 
-		if (root->left == NULL)
+		node->parent = y;
+		if (y == nullptr)
+			root = node;
+		else if (node->data < y->data)
+			y->left = node;
+		else
+			y->right = node;
+
+		if (node->parent == nullptr)
 		{
-			struct node *temp = root->right;
-			free(root);
-			return temp;
-		}
-		else if (root->right == NULL)
-		{
-			struct node *temp = root->left;
-			free(root);
-			return temp;
+			node->color = 0;
+			return;
 		}
 
-		struct node *temp = minValueNode(root->right);
+		if (node->parent->parent == nullptr)
+			return;
 
-		root->key = temp->key;
-		root->count = temp->count;
-
-		temp->count = 0;
-
-		root->right = deleteNode(root->right, temp->key);
+		insertFix(node);
 	}
-	return root;
-}
+
+	void printTree()
+	{
+		if (root)
+			printHelper(this->root, "", true);
+	}
+};
 
 int main()
 {
-	struct node *root = NULL;
-	root = insert(root, 10);
-	root = insert(root, 18);
-	root = insert(root, 7);
-	root = insert(root, 15);
-	root = insert(root, 16);
-	root = insert(root, 30);
-	root = insert(root, 25);
-	root = insert(root, 40);
-	root = insert(root, 60);
-
-
-	cout << "Preorder traversal of the given tree "<< endl;
-	inorder(root);
-
-	// cout << "\nDelete 20\n";
-	// root = deleteNode(root, 20);
-	// cout << "Preorder traversal of the modified tree \n";
-	// inorder(root);
-
-	// cout << "\nDelete 12\n";
-	// root = deleteNode(root, 12);
-	// cout << "Preorder traversal of the modified tree \n";
-	// inorder(root);
-
-	// cout << "\nDelete 9\n";
-	// root = deleteNode(root, 9);
-	// cout << "Preorder traversal of the modified tree \n";
-	// inorder(root);
-
-	return 0;
+	RedBlackTree bst;
+	while (1)
+    {
+        cout << "Choose:\t1.Insert\t2.Display\t3.Exit\t";
+        int key;
+        cin >> key;
+        switch (key)
+        {
+        case 1:
+            int ele;
+            cout << "Insert: ";
+            cin >> ele;
+            bst.insert(ele);
+            cout << "Traversal of tree constructed is\n";
+            bst.printTree();
+            break;
+        case 2:
+            cout << "Traversal of tree constructed is";
+            bst.printTree();
+            break;
+        case 3:
+            exit(0);
+            break;
+        }
+    }
 }
